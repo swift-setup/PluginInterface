fcf75e9c9a67da1fb5a49b8bf578b4a423cb67aa
sirily11 <sirily1997@gmail.com>
2023-01-21 17:37:20 +0800

    fix: add build view

--- Sources/PluginInterface/PluginInterface.swift
+++ Sources/PluginInterface/PluginInterface.swift
@@ -1,11 +1,20 @@
+import SwiftUI
+
 public protocol PluginInterfaceProtocol {
     func run() -> String
 }
 
-open class PluginBuilder {
+
+public protocol PluginUIInterfaceProtocol: PluginInterfaceProtocol {
+    associatedtype Body: View
+    @ViewBuilder @MainActor var view: Self.Body { get }
+}
+
+
+open class PluginBuilder<T> {
     public init() {}
 
-    open func build() -> PluginInterfaceProtocol {
+    open func build() -> any PluginInterfaceProtocol {
         fatalError("You should override this method.")
     }
 }
